403758971eb45a67a4f5896f4cb53638cb62c7e5
Sven Serneels <svenserneels@gmail.com>
2023-07-17 12:06:05 -0600

    notebook and top level file edits

--- src/RitSpls.jl
+++ src/RitSpls.jl
@@ -4,7 +4,7 @@ using ScikitLearn, Statistics, DataFrames, Parameters, ForwardDiff, CSV
 import ScikitLearn.GridSearch:GridSearchCV
 import ScikitLearnBase: BaseRegressor, BaseEstimator, predict, fit!, fit_transform!, @declare_hyperparameters, is_classifier, clone, transform
 import LinearAlgebra: diagm, norm, eigen
-import Optim: optimize, minimizer, LBFGS    
+import Optim: optimize, minimizer, LBFGS
     include("_preproc_utils.jl")
     include("_rob_utils.jl")
     include("_sreg_utils.jl")
@@ -13,5 +13,7 @@ import Optim: optimize, minimizer, LBFGS
     include("modelmetrics.jl")
     include("spls_algorithms.jl")
     include("spls_sklearn.jl")
-    export RitSpls, GSSPP, wrap, autoscale, set_params_dict!, mad
+    export RitSpls, GSSPP, SPLS, wrap, autoscale, set_params_dict!, mad, fit_if,
+        sid, mean_squared_error, mean_absolute_error, r2, maximum_absolute_error,
+        kstepLTS, l1median, autoscale
 end
